dbee50ab959d5f44bd99ad20a25284caa6c6eaca
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-11-17 21:28:46 -0800

    Update Q2.sql

--- A2/SQL/Q2.sql
+++ A2/SQL/Q2.sql
@@ -1,4 +1,4 @@
 SELECT MFR_ID, PRODUCT_ID, DESCRIPTION, (PRICE * QTY_ON_HAND) AS dollar_value
 FROM PRODUCTS
-WHERE (PRICE * QTY_ON_HAND) > 20000.00
+WHERE (PRICE * QTY_ON_HAND) > 20000.00 --in oracle, setting dollar_value on the WHERE clause is not supported
 ORDER BY dollar_value DESC; 
