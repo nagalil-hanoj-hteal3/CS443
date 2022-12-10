c30425265b83157e812fcf79e2fcf05261844f72
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:06:44 -0800

    Update Q9.sql

--- A3/SQL/Q9.sql
+++ A3/SQL/Q9.sql
@@ -1,3 +1,5 @@
+--9)	Do question 8, still using the subquery but do not use the “all” keyword.
+
 SELECT customers.CUST_NUM, salesreps.empl_num, customers.CREDIT_LIMIT
 FROM customers, salesreps
 WHERE customers.CUST_REP = salesreps.empl_num AND customers.CREDIT_LIMIT >
