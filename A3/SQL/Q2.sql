7e1ecf88a620e9414450f8c87f9c7b07c0f307a3
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:03:32 -0800

    Update Q2.sql

--- A3/SQL/Q2.sql
+++ A3/SQL/Q2.sql
@@ -1,3 +1,5 @@
+--2)	Determine how many orders were made in 2022.  Return the number of rows that meet this condition.
+
 SELECT COUNT(*) AS “ORDERS FROM 2022”
 FROM ORDERS
 WHERE ORDERS.ORDER_DATE LIKE ‘%22’;
