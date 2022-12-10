501dd2b3b1f38704e5db48f18bbedf450511aab4
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:06:15 -0800

    Update Q8.sql

--- A3/SQL/Q8.sql
+++ A3/SQL/Q8.sql
@@ -1,3 +1,5 @@
+--8)	Use a subquery and using the “all” keyword to find the customer number, Salesrep id, and CreditLimit of every customer whose CreditLimit is larger than the CreditLimit of all of the customers of sales rep number 109. 
+
 SELECT customers.CUST_NUM, salesreps.empl_num, customers.CREDIT_LIMIT
 FROM customers, salesreps
 WHERE customers.CUST_REP = salesreps.empl_num AND customers.CREDIT_LIMIT > ALL
