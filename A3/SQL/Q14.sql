39feb1c34030533e535244401569992618c43207
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:11:03 -0800

    Create Q14.sql

---
+++ A3/SQL/Q14.sql
@@ -0,0 +1,7 @@
+--14)	Insert the following information into the OFFICES table:
+--Office: 10 	City: Miami	Region: Southern	Manager: 106	Sales: 0
+-- -	Target should be Null. Do not use explicit Null for the target in your insert statement.
+--Recreate the orders table after doing the insert
+
+INSERT INTO offices (office, city, region, mgr, sales)
+VALUES (10, 'Miami', 'Southern', 106, 0);
