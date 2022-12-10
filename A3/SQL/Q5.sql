d5fd672bd554653922b9eb85c9190077a1e32bf2
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:04:52 -0800

    Update Q5.sql

--- A3/SQL/Q5.sql
+++ A3/SQL/Q5.sql
@@ -1,3 +1,5 @@
+--5)	What is the average sale amount for each sale reps in each office. Null should be ignored 
+
 SELECT AVG(salesreps.sales) AS "Avg_Sale_Amount", salesreps.rep_office
 FROM salesreps, offices
 WHERE salesreps.rep_office = offices.office and salesreps.sales is not null
