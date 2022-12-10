6b02dd5f94beb64b9534bd149b4d1a990295043a
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-12-10 13:05:23 -0800

    Update Q6.sql

--- A3/SQL/Q6.sql
+++ A3/SQL/Q6.sql
@@ -1,3 +1,5 @@
+--6)	For each salesrep that has made an order, list the minimum, maximum and average order amount for all their orders.  Include only those orders made anytime from 2020-2021.  Omit from the list any salesrep that has only made 1 order in this time frame.  Sort the results by Empl_Num.
+
 SELECT salesreps.empl_num, MIN(ORDERS.AMOUNT) AS "Min_Amount", MAX(ORDERS.AMOUNT) AS "Max_Amount", AVG(ORDERS.AMOUNT) AS "Avg_Amount"
 FROM salesreps, ORDERS
 WHERE salesreps.empl_num = ORDERS.REP AND EXTRACT(year FROM ORDERS.ORDER_DATE) BETWEEN 2020 AND 2021 --include orders made anytime from 2020-2021
